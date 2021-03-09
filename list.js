a091e61def3258ce61a7e768c8b22ef4353b2448
salome775 <59119025+salome775@users.noreply.github.com>
2021-03-09 18:11:55 +0400

    Add files via upload

---
+++ list.js
@@ -0,0 +1,16 @@
+ function darkColor(color){
+    document.body.style.background = '#292b2c';
+    document.getElementById('container').style.background = '#e1e1dd';
+}
+
+
+function lightColor(color){
+    document.body.style.background = '#e7eae5';
+    document.getElementById('container').style.background = '#292b2c';
+}
+
+
+
+
+
+
